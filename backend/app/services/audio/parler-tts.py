fb4c872ebc64afcfac3a03de722768245638004f
Frédéric TAIEB <frederictaieb@Frederics-MacBook-Pro.local>
2025-06-13 21:57:12 +0200

    Amelioratin de la gestion de la radio, radio.liq

--- backend/app/services/audio/parler-tts.py
+++ backend/app/services/audio/parler-tts.py
@@ -2,9 +2,11 @@ import torch
 from parler_tts import ParlerTTSForConditionalGeneration
 from transformers import AutoTokenizer
 import soundfile as sf
+import numpy as np
+from pydub import AudioSegment
 
 voices = {
-    "alain": ["An old man speaks with a neutral tone and natural pace. He is calm, relaxed and talks slowly. The speech is clear and expressive, recorded in studio quality. The speech is in French. There is no accent. there is a pause at beginning and end. Add some vintage noise The speech is in French. There is no accent. there is a pause at beginning and end."],
+    "alain": ["An old man speaks with a neutral tone and natural pace. He is calm, relaxed. The speech is in French. The speech is clear and expressive, recorded in studio quality. There is no accent. there is a 3 seconds silence at the beginning and at the end."],
     "pierre": [""],
     "thomas": [""],
 }
@@ -24,8 +26,16 @@ def generate_audio(prompt: str, voice: str = "alain") -> bytes:
 
     generation = model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids)
     audio_arr = generation.cpu().numpy().squeeze()
-    return audio_arr
+    audio_int16 = (audio_arr * 32767).astype(np.int16)
+    audio_segment = AudioSegment(
+       audio_int16.tobytes(),
+       frame_rate=44000,
+       sample_width=2,  # 16 bits = 2 bytes
+       channels=1
+    )
+    audio_segment.export("w01.mp3", format="mp3")
+
+    return audio_segment
 
 if __name__ == "__main__":
-    audio_arr = generate_audio("Bonjour et bienvenue dans cette démonstration de synthèse.")
-    sf.write("parler_tts_fr.wav", audio_arr, 16000)
+    generate_audio(f"Ça, t'as raison. Cette ville devient infernale... Moi et ma Madame on vendra cet endroit un jour prochain, je pense. Si on pouvait acheter un gentil petit restaurant en centre-ville ou un relais, ça nous irait.")
